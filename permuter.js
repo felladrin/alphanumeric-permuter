c11064ccb9db8383ae8b7b8ad898364db912126a
Victor Nogueira <felladrin@gmail.com>
2016-07-17 20:30:21 -0300

    Refactor the code to make it easier to understand

--- permuter.js
+++ permuter.js
@@ -1,38 +1,77 @@
-var result = [];
-var count = 0;
+var Permutation = {
+    result: [],
+    possibilities: 0,
 
-function permute(p, l)
-{
-    if (!l.length)
-    {
-        result[count++] = p.join('');
+    /**
+     * Executes the permutation over a given string.
+     * @param {string} str
+     */
+    execute: function (str) {
+        var charactersCount = str.length;
+        this.result = [];
+        this.possibilities = this.factorial(charactersCount);
+
+        if (charactersCount > 8) {
+            alert("Can't permute a " + charactersCount + "-characters string! Your browser would blow up with all the " + this.possibilities + " possible permutations!");
+            return;
+        }
+
+        var arrayOfChars = str.split('');
+        this.permute(arrayOfChars);
+        this.printResult();
+    },
+
+    /**
+     * Permutes a given array of characters and add to the 'result' array.
+     * @param {Array} arrayOfChars
+     * @param {Array} [permutation]
+     */
+    permute: function (arrayOfChars, permutation) {
+        if (typeof permutation === 'undefined') {
+            permutation = [];
+        }
+
+        if (arrayOfChars.length == 0) {
+            this.result.push(permutation.join(''))
+        }
+        else {
+            for (var i = 0; i < arrayOfChars.length; i++) {
+                var permutationCopy = permutation.slice(0);
+                permutationCopy.push(arrayOfChars[i]);
+
+                var arrayOfCharsCopy = arrayOfChars.slice(0);
+                arrayOfCharsCopy.splice(i, 1);
+
+                this.permute(arrayOfCharsCopy, permutationCopy);
+            }
+        }
+    },
+
+    /**
+     * Calculates the factorial of a given number.
+     * @param {number} num
+     * @returns {number}
+     */
+    factorial: function (num) {
+        var val = 1;
+        for (var i = 2; i <= num; i++) {
+            val *= i;
+        }
+        return val;
+    },
+
+    /**
+     * Prints the result.
+     */
+    printResult: function () {
+        document.getElementById('possibilities').innerHTML = this.possibilities.toString();
+        var result = document.getElementById('result');
+        result.innerHTML = '';
+
+        this.result.forEach(function (permutation) {
+            var li = document.createElement('li');
+            li.innerHTML = permutation;
+            result.appendChild(li);
+        });
     }
-    else
-    {
-        for (var i in l)
-            permute(p.concat(l[i]), l.slice(0, l.indexOf(l[i])).concat(l.slice(l.indexOf(l[i]) + 1, l.length)));
-    }
-}
-
-function factorial(num)
-{
-    var val = 1;
-    for (var i = 2; i <= num; i++)
-        val = val * i;
-    return val;
-}
-
-function calcPermutations(num)
-{
-    if (num > 9)
-    {
-        alert("Can't permute a word with more then 9 letters... Your browser would blow up.");
-        return;
-    }
-
-    document.getElementById('possibilities').innerHTML = factorial(num);
-    result = [];
-    count = 0;
-    permute([], document.getElementById('word').value);
-    document.getElementById('result').value = result.join('\n');
-}
+};
